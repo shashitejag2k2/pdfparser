b63d78d3cf3f9af22a5b542858da51eb0659e7dc
shashi teja <shashitejag2k2@gmail.com>
2024-05-01 01:01:03 +0530

    final commit

--- index.js
+++ index.js
@@ -4,32 +4,25 @@ const PDFParser = require("pdf-parse");
 const fs = require("fs");
 const path = require("path");
 const morgan = require("morgan");
-const cors = require("cors"); // Import cors middleware
+const cors = require("cors"); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Set up Multer for handling file uploads
+
 const upload = multer({ dest: "uploads/" });
 
-// Use morgan for logging HTTP requests
+
 app.use(morgan("dev"));
 
-// Enable CORS
 app.use(cors());
 
-// Serve HTML file for uploading PDF
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "upload.html"));
-});
-
-// Handle PDF file upload and parsing
 app.post("/upload", upload.single("pdfFile"), (req, res) => {
   const pdfPath = req.file.path;
 
   console.log("Received PDF file:", pdfPath);
 
-  // Function to parse PDF file and extract text content
+ 
   function parsePDF(pdfPath) {
     return new Promise((resolve, reject) => {
       fs.readFile(pdfPath, (err, data) => {
@@ -50,13 +43,13 @@ app.post("/upload", upload.single("pdfFile"), (req, res) => {
     });
   }
 
-  // Parse PDF and send the extracted sections as JSON object
+  
   parsePDF(pdfPath)
     .then((content) => {
-      // Remove the uploaded file after parsing
+      
       fs.unlinkSync(pdfPath);
 
-      // Extract education, skills, and experience using regular expressions
+      
       const educationRegex = /E D U C A T I O N\n([\s\S]+?)(?=\n\n\d{4}|$)/;
       const skillsRegex = /S K I L L S\n([\s\S]+?)(?=\n\n[A-Z]|$)/;
       const experienceRegex =
@@ -66,7 +59,7 @@ app.post("/upload", upload.single("pdfFile"), (req, res) => {
       const skillsMatch = content.match(skillsRegex);
       const experienceMatch = content.match(experienceRegex);
 
-      // Function to extract years and headings from experience section
+      
       function extractExperience(experience) {
         if (!experience) return [];
         const experiences = experience.match(/\d{4}([^•]+•)/g);
@@ -75,7 +68,7 @@ app.post("/upload", upload.single("pdfFile"), (req, res) => {
           : [];
       }
 
-      // Function to split skills into separate items
+     
       function splitSkills(skills) {
         if (!skills) return "";
         return skills
@@ -84,7 +77,7 @@ app.post("/upload", upload.single("pdfFile"), (req, res) => {
           .join(", ");
       }
 
-      // Construct JSON object with extracted sections
+      
       const resumeData = {
         education: educationMatch ? educationMatch[1].trim() : "",
         skills: skillsMatch ? splitSkills(skillsMatch[1].trim()) : "",
@@ -101,7 +94,7 @@ app.post("/upload", upload.single("pdfFile"), (req, res) => {
     });
 });
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
